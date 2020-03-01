1a3c630ac0180c7f91feaddb355e90aa76807d9d
nic britz <nico.britz@outlook.com>
2020-03-01 14:15:08 +0000

    [BugFix] - tiedied up code and removed unessery comments

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,21 +1,23 @@
-
 //-------- first simple test for Jasmine ------>
 function helloJasmine() {
   return "Hello Jasmine Testing";
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
   //-------- hover and click on game buttons ------>
-$("#game-buttons > div").hover(
-  function() {
-    $(this).addClass("game-button-hovered");
-  },
-  function() {
-    $(this).removeClass("game-button-hovered");
-  }
-).click(function(){
-  console.log(this.dataset.button);
-  $(`#${this.dataset.button}-classic-audio`).get(0).play();
+  $("#game-buttons > div")
+    .hover(
+      function() {
+        $(this).addClass("game-button-hovered");
+      },
+      function() {
+        $(this).removeClass("game-button-hovered");
+      }
+    )
+    .click(function() {
+      
+      $(`#${this.dataset.button}-classic-audio`)
+        .get(0)
+        .play();
+    });
 });
-})
-
