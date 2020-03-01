f0752a311694ac3ba0c9d62ffa3380372f75b2af
nic britz <nico.britz@outlook.com>
2020-03-01 13:07:25 +0000

    adds interactivity to the gameplay buttons

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,22 @@
-console.log('My JS is linked now');
+console.log("My JS is linked now");
 
 //-------- first simple test for Jasmine ------>
-function helloJasmine(){
-    return "Hello Jasmine Testing"
+function helloJasmine() {
+  return "Hello Jasmine Testing";
+}
+
+//-------- hover and click on game buttons ------>
+function helloJasmine() {
+  $("#game-buttons > div")
+    .hover(
+      function() {
+        $(this).addClass("game-button-hovered");
+      },
+      function() {
+        $(this).removeClass("game-button-hovered");
+      }
+    )
+    .click(function() {
+      console.log(this);
+    });
 }
