9f66041c7c43ddb04e53752824b4e434e4edc4e2
nic britz <nico.britz@outlook.com>
2020-03-01 19:23:22 +0000

    [Addition]- Adds auto play for the computer

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,46 +3,97 @@ function helloJasmine() {
   return "Hello Jasmine Testing";
 }
 
+let gameSpeed = 1000;
+let roundArray = [
+  "blue",
+  "green",
+  "red",
+  "yellow",
+  "blue",
+  "green",
+  "red",
+  "yellow"
+];
+
 //checks if it is a touch screen device
-let isTouch =
-  !!("ontouchstart" in window) || window.navigator.msMaxTouchPoints > 0;
 
-$(document).ready(function() {
-  // hover effect if its not a touch device
-  if (!isTouch) {
-    $("#game-buttons > div").hover(
-      function() {
-        $(this).addClass("game-button-hovered");
-      },
-      function() {
-        $(this).removeClass("game-button-hovered");
-      }
-    );
-  }
+$(document).ready(function() {});
 
-  // Game button clicked
-  $("#game-buttons > div").click(function() {
-    buttonPressed(this);
-  });
+//-------------temp play button
+$("#game-center-circle").click(function() {
+  computerRound(gameSpeed, roundArray);
 });
+
+//Play Sound
+function playSound(sound) {
+  // restarting it if its already playing
+  $(`audio`).get(0).currentTime = 0;
+
+  $(`#${sound}-classic-audio`)
+    .get(0)
+    .play();
+}
 
 // game play Button Pressed
 function buttonPressed(button) {
   // variables
-  let btn = $(`#${button.id}`);
-  let btnData = button.dataset.button;
+  let btn = $(`#game-button-${button}`);
 
   // adds pressed class and removers shortly after
   btn.addClass("game-button-pressed");
 
   setTimeout(function() {
     btn.removeClass("game-button-pressed");
-  }, 100);
+  }, 200);
+}
 
-  // Plays appropriate sound restarting it if its already playing
-  $(`audio`).get(0).currentTime = 0;
+// computer Round
+function computerRound(speed, roundList) {
+  setTimeout(function() {
+    let count = 0;
 
-  $(`#${btnData}-classic-audio`)
-    .get(0)
-    .play();
+    roundList.forEach((value, i) => {
+      setTimeout(function() {
+        autoPlay(value);
+        count++;
+        if (count == roundList.length) {
+          playerRound(roundArray);
+        }
+      }, i * speed);
+    });
+  }, speed);
+}
+
+//Player round
+function playerRound(roundList) {
+  let playerList = [];
+  setTimeout(function() {
+    let isTouch =
+      !!("ontouchstart" in window) || window.navigator.msMaxTouchPoints > 0;
+    // hover effect if its not a touch device
+    if (!isTouch) {
+      $("#game-buttons > div").hover(
+        function() {
+          $(this).addClass("game-button-hovered");
+        },
+        function() {
+          $(this).removeClass("game-button-hovered");
+        }
+      );
+    }
+    console.log("Done and Dusted!");
+    // Game button clicked
+    $("#game-buttons > div").click(function() {
+      playSound(this.dataset.button);
+      buttonPressed(this.dataset.button);
+      playerList.push(this.dataset.button);
+      console.log(playerList);
+    });
+  }, 2000);
+}
+
+// auto play
+function autoPlay(button) {
+  buttonPressed(button);
+  playSound(button);
 }
