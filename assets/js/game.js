72cd1bb83912dc389f5bd5edbcb05ace47440c14
nic britz <nico.britz@outlook.com>
2020-03-05 20:33:22 +0000

    [BugFix] fixed player turn result checker

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,104 +1,139 @@
 //-------- first simple test for Jasmine ------>
-// function helloJasmine() {
-//   return "Hello Jasmine Testing";
-// }
+function helloJasmine() {
+  return "Hello Jasmine Testing";
+}
+
+// Button constants
+const gameCenterCircle = document.getElementById("game-center-circle");
+
+// Audio constants
+const allAudio = document.getElementsByTagName("audio");
+const greenClassicAudio = document.getElementById("green-classic-audio");
+const redClassicAudio = document.getElementById("red-classic-audio");
+const yellowClassicAudio = document.getElementById("yellow-classic-audio");
+const blueClassicAudio = document.getElementById("blue-classic-audio");
+
+// Game colour buttons
+const allGameButtons = $("#game-buttons > div");
+const greenGameButton = document.getElementById("green-game-button");
+const redGameButton = document.getElementById("red-game-button");
+const yellowGameButton = document.getElementById("yellow-game-button");
+const blueGameButton = document.getElementById("blue-game-button");
 
 let gameSpeed = 1000;
 let roundArray = [];
 let playerTurn = 0;
 let roundLen = 1;
+let playerActive = false;
+let playerTurnArray = [];
+
+/* check if touch screen -
+ * https://stackoverflow.com/questions/17233804/how-to-prevent-sticky-hover-effects-for-buttons-on-touch-devices/28058919
+ */
+let isTouch =
+  !!("ontouchstart" in window) || window.navigator.msMaxTouchPoints > 0;
 
 $(document).ready(function() {
+  buttonPressed(blueGameButton);
+
   //--------------------------------------------------------------- temp play button -----//
-  $("#game-center-circle")
-    .unbind()
-    .click(function() {
-      computerRound(gameSpeed, roundArray);
-    });
+  gameCenterCircle.addEventListener("click", function() {
+    computerPlayRound(gameSpeed);
+  });
 
   //--------------------------------------------------------------- Play Sound -------------//
   function playSound(sound) {
-    // restarting it if its already playing
-    $(`audio`).get(0).currentTime = 0;
-
-    $(`#${sound}-classic-audio`)
-      .get(0)
-      .play();
+    // restart any audio already playing
+    for (var i = 0; i < allAudio.length; i++) {
+      allAudio[i].currentTime = 0;
+    }
+    // play appropriate audio file
+    switch (sound) {
+      case "green":
+        greenClassicAudio.play();
+        break;
+      case "red":
+        redClassicAudio.play();
+        break;
+      case "yellow":
+        yellowClassicAudio.play();
+        break;
+      case "blue":
+        blueClassicAudio.play();
+        break;
+    }
   }
 
-  //--------------------------------------------------------------- game play Button Pressed
+  //--------------------------------------------------------------- Game colour buttons pressed
+
   function buttonPressed(button) {
-    let btn = $(`#game-button-${button}`);
-
-    // adds pressed class and removers shortly after
-    btn.addClass("game-button-pressed");
-
-    setTimeout(function() {
-      btn.removeClass("game-button-pressed");
-    }, 200);
+    switch (button) {
+      case "green":
+        greenGameButton.classList.add("game-button-pressed");
+        setTimeout(function() {
+          greenGameButton.classList.remove("game-button-pressed");
+        }, 200);
+        break;
+      case "red":
+        redGameButton.classList.add("game-button-pressed");
+        setTimeout(function() {
+          redGameButton.classList.remove("game-button-pressed");
+        }, 200);
+        break;
+      case "yellow":
+        yellowGameButton.classList.add("game-button-pressed");
+        setTimeout(function() {
+          yellowGameButton.classList.remove("game-button-pressed");
+        }, 200);
+        break;
+      case "blue":
+        blueGameButton.classList.add("game-button-pressed");
+        setTimeout(function() {
+          blueGameButton.classList.remove("game-button-pressed");
+        }, 200);
+        break;
+    }
   }
 
   //-------------------------------------------------------------------------- computer Round
-  function computerRound(speed, roundList) {
-    randomizeRound();
-    setTimeout(function() {
-      let count = 0;
-      // delays between each iteration
-      //https://travishorn.com/delaying-foreach-iterations-2ebd4b29ad30
-      roundList.forEach(function(value, ind) {
-        setTimeout(function() {
-          autoPlay(value);
-          count++;
-          if (count == roundList.length) {
-            playerRound(roundArray);
-          }
-        }, ind * speed);
-      });
-    }, speed);
-  }
+  // function computerRound(speed, roundList) {
+  //   // randomizeRound();
+  //   setTimeout(function() {
+  //     let count = 0;
+  //     // delays between each iteration
+  //     roundList.forEach(function(value, ind) {
+  //       setTimeout(function() {
+  //         autoPlay(value);
+  //         count++;
+  //         if (count == roundList.length) {
+  //           playerRound();
+  //         }
+  //       }, ind * speed);
+  //     });
+  //   }, speed);
+  // }
 
   // ------------------------------------------------------------------------------ Player round
-  function playerRound() {
-    let playerTurn = 0;
-    setTimeout(function() {
-      //checks if it is a touch screen device source:
-      //https://stackoverflow.com/questions/17233804/how-to-prevent-sticky-hover-effects-for-buttons-on-touch-devices/28058919
-      let isTouch =
-        !!("ontouchstart" in window) || window.navigator.msMaxTouchPoints > 0;
+  // function playerRound() {
+  //   playerTurn = 0;
+  //   setTimeout(function() {
+  //     playerActive = true;
+  //     console.log("Ready For Player");
 
-      // hover effect if its not a touch device
-      if (!isTouch) {
-        // $("#game-buttons > div").hover(
-        //   function() {
-        //     $(this).addClass("game-button-hovered");
-        //   },
-        //   function() {
-        //     $(this).removeClass("game-button-hovered");
-        //   }
-        // );
-      }
-      console.log("Done and Dusted!");
+  // Game button clicked https://stackoverflow.com/questions/14969960/jquery-click-events-firing-multiple-times
+  // $("#game-buttons > div")
+  //   .unbind()
+  //   .click(function() {
+  //     let pressed = this.dataset.button;
 
-      // Game button clicked https://stackoverflow.com/questions/14969960/jquery-click-events-firing-multiple-times
-      $("#game-buttons > div")
-        .unbind()
-        .click(function() {
-          let pressed = this.dataset.button;
-
-          playSound(pressed);
-          buttonPressed(pressed);
-          checkResult(pressed, playerTurn);
-          playerTurn += 1;
-          console.log("player turn " + playerTurn);
-        });
-    }, 2000);
-  }
-
-  // auto play
-  function autoPlay(button) {
-    buttonPressed(button);
-    playSound(button);
-  }
+  // playSound(pressed);
+  // buttonPressed(pressed);
+  // checkResult(pressed, playerTurn);
+  //   playerTurn += 1;
+  //   console.log("player turn " + playerTurn);
+  // });
+  // }, 1500);
+  // }
 
   function gameOver() {
     alert("Game Over");
@@ -106,33 +141,125 @@ $(document).ready(function() {
     location.reload();
   }
 
-  // randomize next turn
-  function randomizeRound() {
-    playerTurn = 0;
-    choices = ["red", "green", "blue", "yellow"];
+  // // check result
+  // function checkResult(buttonPressed, turn) {
+  //   if (roundLen < roundArray.length) {
+  //     if (buttonPressed === roundArray[turn]) {
+  //       console.log("pass");
+  //       roundLen++;
+  //     } else {
+  //       gameOver();
+  //       return;
+  //     }
+  //     return console.log(
+  //       `Pressed: ${buttonPressed}  ==== compared ${roundArray[turn]}`
+  //     );
+  //   } else {
+  //     setTimeout(function() {
+  //       roundArray.push(generateRound());
+  //       console.log(roundArray);
+  //       computerPlayRound(gameSpeed);
+  //     }, 2000);
+  //   }
+  // }
 
-    roundArray.push(choices[Math.floor(Math.random() * choices.length)]);
-    console.log(roundArray);
-  }
+  //========================================= GamePlay ===========================================>>>
 
-  // check result
-  function checkResult(buttonPressed, turn) {
+  // Game button hover --------->
+  allGameButtons.hover(mouseInBtn, mouseOutBtn);
 
-    if (roundLen < roundArray.length) {
-      if (buttonPressed === roundArray[turn]) {
-        console.log("pass");
-        roundLen++
-      } else {
-        gameOver();
-        return;
+  //in
+  function mouseInBtn() {
+    if (playerActive) {
+      // hover effect if its not a touch device
+      if (!isTouch) {
+        this.classList.add("game-button-hovered");
       }
-      return console.log(
-        `Pressed: ${buttonPressed}  ==== compared ${roundArray[turn]}`
-      );
-    }else{
-      setTimeout(function() {
-        computerRound(gameSpeed, roundArray);
-      }, 2000);
     }
   }
+
+  //out
+  function mouseOutBtn() {
+    if (playerActive) {
+      // hover effect if its not a touch device
+      if (!isTouch) {
+        this.classList.remove("game-button-hovered");
+      }
+    }
+  }
+
+  //-----------------------------<
+
+  // Game button click --------->
+  function playerRound() {
+    allGameButtons.unbind().click(function() {
+      let pressed = this.dataset.button;
+      if (playerActive) {
+        playSound(pressed);
+        buttonPressed(pressed);
+        checkResult(pressed);
+      }
+    });
+  }
+
+  ////////////////////////////////////////////////////////////////////
+//players current place in round
+let playerChoice = 0;
+
+  //computer plays
+  function computerPlayRound(speed) {
+    //current choice
+    playerChoice = 0;
+    //generate next round element
+    roundArray.push(generateRound());
+
+    //Player is deactivated
+    playerActive = false;
+
+    //Computer play Round
+    setTimeout(function() {
+      // delays between each round iteration
+      roundArray.forEach(function(value, ind) {
+        setTimeout(function() {
+          buttonPressed(value);
+          playSound(value);
+          if (ind + 1 === roundArray.length) {
+            console.log("computer round ended");
+            playerActive = true;
+            playerRound();
+          }
+        }, ind * speed);
+      });
+    }, speed);
+  }
+
+  
+
+  // check the player result
+  function checkResult(button) {
+    //Check if the players last last choice is correct
+    if (button === roundArray[playerChoice]) {
+      console.log("yes");
+      if (playerChoice + 1 === roundArray.length) {
+        console.log("Round WON!!!!")
+        //wait a couple to give clear indication of next round
+        setTimeout(function(){computerPlayRound(gameSpeed)},2000);
+        return;
+      } else {
+        playerChoice++;
+        return true;
+      }
+    } else {
+      console.log("nope");
+      gameOver();
+      return false;
+    }
+  }
+
+  function generateRound() {
+    let choices = ["red", "green", "blue", "yellow"];
+    return choices[Math.floor(Math.random() * choices.length)];
+  }
+
+  
 });
